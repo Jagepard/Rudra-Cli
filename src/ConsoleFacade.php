3423a677dffae44f0b0e9f2495ba6cd505ce2c7c
Jagepard <dankorot@gmail.com>
2021-03-06 10:23:19 +0300

    fix

--- src/ConsoleFacade.php
+++ src/ConsoleFacade.php
@@ -15,7 +15,7 @@ use Rudra\Container\Traits\FacadeTrait;
  * @method static void   printer(string $text, string $fg = "default", string $bg = "default")
  * @method static string reader()
  * @method static void   addCommand($name, $command)
- * @method static void   run($inputArgs)
+ * @method static void   invoke($inputArgs)
  * @method static array  getRegistry()
  *
  * @see Router
