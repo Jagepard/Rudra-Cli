ca5b836161a57ca5a5b7a9a078fbe04c1b2f7ca9
Jagepard <jagepard@yandex.ru>
2025-12-26 15:38:03 +0300

    fix

--- src/ConsoleInterface.php
+++ src/ConsoleInterface.php
@@ -1,7 +1,5 @@
 <?php
 
-declare(strict_types = 1);
-
 /**
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
